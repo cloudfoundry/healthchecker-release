6acbaa56194755654e3de18f2a03e27a4b469a33
Renee Chu <reneec@vmware.com>
2023-01-13 23:37:19 +0000

    use ginkgo v1

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ set -e -u
 go version # so we see the version tested in CI
 
 if ! [ $(type -P "ginkgo") ]; then
- go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo@latest
+ go install -mod=mod github.com/onsi/ginkgo@v1
 fi
 
 SCRIPT_PATH="$(cd "$(dirname "${0}")" && pwd)"
