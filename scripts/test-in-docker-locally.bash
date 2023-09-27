1310cf320cdb05f1e38745fe01cc2394e1e67611
Amin Jamali <ajamali@vmware.com>
2023-09-27 13:23:19 +0000

    Remove dependency on .envrc
    - Reorgnanize scripts so that it's clear which scripts are ran
    inside ofg docker containers and which are ran in the local workstation
    - ./scripts/test-in-docker-locally.bash -> Helper to run Everything
    - ./scripts/create-docker-container.bash -> Create a docker container to
      test and run things manually

--- scripts/test-in-docker-locally.bash
+++ scripts/test-in-docker-locally.bash
@@ -3,8 +3,11 @@
 set -eu
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CI="${THIS_FILE_DIR}/../../wg-app-platform-runtime-ci"
+. "$CI/shared/helpers/git-helpers.bash"
+REPO_NAME=$(git_get_remote_name)
 
-"${THIS_FILE_DIR}/docker/container.bash" -d
+"${THIS_FILE_DIR}/create-docker-container.bash" -d
 
 docker exec $REPO_NAME-docker-container '/repo/scripts/docker/test.bash' "$@"
 docker exec $REPO_NAME-docker-container '/repo/scripts/docker/lint.bash'
